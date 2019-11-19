13b44acc359f4f0f63aa3209d45a1a95425f8576
Rod Vagg <rod@vagg.org>
2019-11-19 13:32:57 +1100

    initial, direct workers support

--- resources/mocha-run.js
+++ resources/mocha-run.js
@@ -1,50 +1,78 @@
-/* globals Worker Mocha mocha polendinaEnd polendinaLog polendinaWrite */
+/* globals self Worker WorkerGlobalScope postMessage importScripts Mocha mocha polendinaEnd polendinaLog polendinaWrite */
 
+const inPageLoadingWorker = typeof window !== 'undefined' && window.location && window.location.search === '?worker'
+const inWorker = typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope
 let executionQueue = Promise.resolve()
-
-mocha.useColors(true)
 
 function consoleLog (...args) {
   executionQueue = executionQueue.then(polendinaLog(args))
 }
 
-// the well-behaved reporters, like spec, are easy to intercept
-Mocha.reporters.Base.consoleLog = consoleLog
-
-// the poorly behaved ones, not so much, we get both console.log()
-// and process.stdout.write() reporting here
-console.log = function (...args) {
-  try {
-    if (/BrowserStdout.*write/.test(new Error().stack)) {
-      // the BrowserStdout polyfill Mocha ships with that converts process.stdout.write() to console.log()
-      // so we strip out the extra \n that necessarily inserts
-      // args[0] = args[0].replace(/\n$/, '')
-      executionQueue = executionQueue.then(polendinaWrite(args))
+function runWorker () {
+  console.log('starting worker')
+  const worker = new Worker('mocha-run.js')
+  worker.addEventListener('message', (msg) => {
+    if (!Array.isArray(msg.data)) {
       return
     }
-  } catch (err) {}
-  consoleLog.apply(null, args)
-}
 
-function runWorker () {
-  const worker = new Worker('worker.js')
-  worker.addEventListener('message', (msg) => {
-    if (!Array.isArray(msg)) {
-      return
+    if (!self[msg.data[0]]) {
+      console.log.apply(console, msg.data)
+    } else {
+      self[msg.data[0]].apply(null, msg.data.slice(1))
     }
   }, false)
 }
 
-function runPage () {
+if (inWorker) {
+  self.polendinaLog = function (...args) {
+    postMessage(['polendinaLog'].concat(args))
+  }
+
+  self.polendinaWrite = function (...args) {
+    postMessage(['polendinaWrite'].concat(args))
+  }
+
+  self.polendinaEnd = function (...args) {
+    postMessage(['polendinaEnd'].concat(args))
+  }
+
+  polendinaLog('running')
+  importScripts('bundle.js')
+}
+
+if (!inPageLoadingWorker || inWorker) {
+  mocha.useColors(true)
+
+  if (typeof polendinaLog !== 'undefined') {
+    // the well-behaved reporters, like spec, are easy to intercept
+    Mocha.reporters.Base.consoleLog = consoleLog
+
+    // the poorly behaved ones, not so much, we get both console.log()
+    // and process.stdout.write() reporting here
+    console.log = function (...args) {
+      try {
+        if (/BrowserStdout.*write/.test(new Error().stack)) {
+          // the BrowserStdout polyfill Mocha ships with that converts process.stdout.write() to console.log()
+          // so we strip out the extra \n that necessarily inserts
+          // args[0] = args[0].replace(/\n$/, '')
+          executionQueue = executionQueue.then(polendinaWrite(args))
+          return
+        }
+      } catch (err) {}
+      consoleLog.apply(null, args)
+    }
+  } else { // dev case, where this file is loaded directly, not the normal case
+    self.polendinaLog = console.log
+    self.polendinaWrite = console.log
+    self.polendinaEnd = () => console.log('ENDED')
+  }
+
   setTimeout(() => {
     mocha.run().on('end', () => {
       executionQueue.then(polendinaEnd)
     })
   }, 0)
-}
-
-if (window.location.search === '?worker') {
+} else if (inPageLoadingWorker) {
   runWorker()
-} else {
-  runPage()
 }
