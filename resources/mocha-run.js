bfeeba72df76bccaa4aea5fc0b1ba97eb96ebdc9
Rod Vagg <rod@vagg.org>
2020-01-07 19:48:26 +1100

    update deps, make tests run properer, fix mocha.color()

--- resources/mocha-run.js
+++ resources/mocha-run.js
@@ -7,7 +7,12 @@ const { registry, executionQueue, log, setup } = require('./common-run')
 
 function runMocha () {
   mocha.setup({ reporter: registry.argv.mochaReporter, ui: 'bdd' })
-  mocha.useColors(true)
+  // mocha@7 deprecated useColors()
+  if (typeof mocha.color === 'function') {
+    mocha.color(true)
+  } else if (typeof mocha.useColors === 'function') {
+    mocha.useColors(true)
+  }
 
   // the well-behaved reporters, like spec, are easy to intercept
   Mocha.reporters.Base.consoleLog = log.info
